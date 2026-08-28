f18c2cd6e3a01a3e73b3938788b367b18ad302b8
Nada Salah <125830309+NadaSalahAli@users.noreply.github.com>
2026-08-28 14:29:42 +0300

    Add files via upload

---
+++ servo_motor_testing.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo myServo;
+
+void setup() {
+  myServo.attach(9);
+}
+
+void loop() {
+  myServo.write(0);     // Move to 0°
+  delay(1000);
+
+  myServo.write(90);    // Move to 90°
+  delay(1000);
+
+  myServo.write(180);   // Move to 180°
+  delay(1000);
+}
